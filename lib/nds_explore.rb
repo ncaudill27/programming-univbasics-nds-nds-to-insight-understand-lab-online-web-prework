5db9882f6c6acafa21bd71aad80512de31ee3829
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-20 16:27:12 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,14 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp directors_database
 end
 
 def print_first_directors_movie_titles
+  puts "Jaws"
+  puts "Close Encounters of the Third Kind"
+  puts "Raiders of the Lost Ark"
+  puts "E.T. the Extra-terrestrial"
+  puts "Schindler's List"
+  puts "Lincoln"
 end
